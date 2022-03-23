3eeee7d1d1ad3be735a22d1a047ee6e0271ad44e
minnnh <minih@bu.edu>
2022-03-22 22:24:57 -0400

    uodate main

---
+++ application.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect, url_for, render_template, request
+#from flask_restful import Resource, Api, reqparse
+import json
+import sqlite3
+import os
+
+import datetime
+
+application = Flask(__name__)
+#api = Api(application)
+
+@application.route("/")
+def home():
+	return render_template("index.html")
+
+from flask_chat import *
+from flask_device import *
+
+
+if __name__ == '__main__':
+	application.run(debug=True)
